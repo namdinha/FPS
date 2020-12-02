e25e3885bfaa6da15850baec11947c3b3a1d38ee
thewhitesummer <fe_granger@hotmail.com>
2020-12-02 14:28:09 -0300

    simple enemy ai and small improvements

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour {
 
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+
     void Start() {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     void Update() {
-        
+        navMeshAgent.SetDestination(target.position);
     }
 }
