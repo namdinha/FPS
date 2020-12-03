d6385aba912b38f66c173fad12e9fd9ffdf1927c
thewhitesummer <fe_granger@hotmail.com>
2020-12-02 23:24:50 -0300

    deleted print

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -54,7 +54,6 @@ public class EnemyAI : MonoBehaviour {
     }
 
     private void FaceTarget() {
-        print("dhgufihd");
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
