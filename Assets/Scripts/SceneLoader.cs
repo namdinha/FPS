edbf4b2340e45586f653224f6cc6f173223e357c
thewhitesummer <fe_granger@hotmail.com>
2020-12-03 18:14:52 -0300

    implemented buttons functions

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void ReloadScene() {
+        var currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+
+    public void ReloadGame() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
