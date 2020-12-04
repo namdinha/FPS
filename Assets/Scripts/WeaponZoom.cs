c845370ac6829e0597c08115a412d72cd4db6434
thewhitesummer <fe_granger@hotmail.com>
2020-12-04 16:53:23 -0300

    implemented change of sensitivity when zoomed

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,25 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour {
 
     [SerializeField] Camera FPCamera;
     [SerializeField] float zoomedOutFOV = 60f;
     [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float sensitivityIn = 0.5f;
+    [SerializeField] float sensitivityOut = 2f;
 
     bool zoomedInToggle = false;
+    RigidbodyFirstPersonController FPController;
+
+    void Start() {
+        FPController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     void Update() {
         if(Input.GetMouseButtonDown(1)) {
             if(zoomedInToggle == false) {
                 zoomedInToggle = true;
                 FPCamera.fieldOfView = zoomedInFOV;
+                SetSenitivity(sensitivityIn);
             }
             else {
                 zoomedInToggle = false;
                 FPCamera.fieldOfView = zoomedOutFOV;
+                SetSenitivity(sensitivityOut);
             }
         }
+    }
+
+    private void SetSenitivity(float amount) {
+        FPController.mouseLook.XSensitivity = amount;
+        FPController.mouseLook.YSensitivity = amount;
     }
 }
