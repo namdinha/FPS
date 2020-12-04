7efad66fe0697889e2efd51105cf2187af473e19
thewhitesummer <fe_granger@hotmail.com>
2020-12-04 16:08:11 -0300

    added weapon zoom to the player

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour {
+
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+
+    bool zoomedInToggle = false;
+
+    void Update() {
+        if(Input.GetMouseButtonDown(1)) {
+            if(zoomedInToggle == false) {
+                zoomedInToggle = true;
+                FPCamera.fieldOfView = zoomedInFOV;
+            }
+            else {
+                zoomedInToggle = false;
+                FPCamera.fieldOfView = zoomedOutFOV;
+            }
+        }
+    }
+}
