98c2c3f5e6ba763bb4f5288c2dec4e99194c908f
thewhitesummer <fe_granger@hotmail.com>
2020-12-09 05:31:43 -0300

    added ammo pickup

---
+++ Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour {
+
+    [SerializeField] AmmoType ammo;
+    [SerializeField] int amount = 5;
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player") {
+            other.gameObject.GetComponent<Ammo>().IncreaseCurrentAmmo(ammo, amount);
+            Destroy(gameObject);
+        }
+    }
+}
