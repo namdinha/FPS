f49d4e90703f0718ff954cdeef5657efa8438fce
thewhitesummer <fe_granger@hotmail.com>
2020-12-10 07:08:23 -0300

    added flashlight decay

---
+++ Assets/Scripts/FlashlightSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightSystem : MonoBehaviour {
+
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light myLight;
+
+    void Start() {
+        myLight = GetComponent<Light>();
+    }
+
+    void Update() {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    private void DecreaseLightIntensity() {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+
+    private void DecreaseLightAngle() {
+        myLight.spotAngle = Math.Max(minimumAngle, myLight.spotAngle - angleDecay * Time.deltaTime);
+    }
+}
